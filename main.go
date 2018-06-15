ac1fc349e48d79387f21503b7aba96fa65b0ec2f
Kosta <jarlaxe95@gmail.com>
2018-06-16 00:45:18 +0100

    Port 8080

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
 	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
